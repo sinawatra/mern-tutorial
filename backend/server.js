5d9d3b1de386bbe89355d97a7556af52e06f2059
JPSE <watrasinarith@gmail.com>
2024-08-06 15:39:09 +0700

    NEw

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 const port = process.env.port || 5000 
+
+connectDB()
 
 const app = express()
 
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+
+app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
